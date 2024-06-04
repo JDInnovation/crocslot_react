0e29eb85c007b358ac48b7e5abdd14566e6703dc
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-06-04 18:58:07 +0100

    build supply banner

--- src/components/Supply/SupplyBanner.js
+++ src/components/Supply/SupplyBanner.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
 import Banner from '../Utils/Banner';
+import coinImage from '../../assets/token_imagem.png';
+import './SupplyBanner.css';
 
 function SupplyBanner() {
   return (
     <Banner component="supply-banner">
+      <div className="front align-center justify-center d-flex-row justify-around w-100">
+        <div className="front align-center justify-center d-flex-col">
+          <div>
+            <h1>777M</h1>
+          </div>
+          <div>
+            <h2>SUPPLY</h2>
+          </div>
+          <div>
+            <ul>
+              <li>MINT & FREEZE REVOKED</li>
+              <li>LIQUIDITY BURNED</li>
+              <li>NO TAXES</li>
+            </ul>
+
+          </div>
+
+        </div>
+        <div>
+          <img src={coinImage} alt="Coin IMG" className="coin-image"></img>
+
+        </div>
+
+
+      </div>
 
     </Banner>
   );
