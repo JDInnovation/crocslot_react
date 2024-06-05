213d8819222dd3e593187d6024d6fe901889dbdd
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-06-05 20:41:12 +0100

    change communiy banner

--- src/components/Community/CommunityBanner.js
+++ src/components/Community/CommunityBanner.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Banner from '../Utils/Banner';
 
+import './CommunityBanner.css';
+
 function CommunityBanner() {
   return (
-    <Banner component="buy-now-banner">
-      <div className="banner community-banner">
-        <div className="d-flex-col w-100 align-center justify-center justify-between">
+    <Banner component="community-banner">
+     
+        <div className="d-flex-col text-center align-center justify-center justify-between">
           <h1>HOW TO BUY?</h1>
           <h3>1. Download Phantom wallet</h3>
           <h3>2. Purchase or Receive $Solana</h3>
@@ -14,7 +16,7 @@ function CommunityBanner() {
           <h3>4. Connect wallet and swap for $CROKI</h3>
         </div>
 
-      </div>
+      
     </Banner>
   );
 }
