86a09e039220bab4717fef9d4dd3b87e7d2af8cd
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-06-05 18:26:47 +0100

    Mobile changes

--- src/components/Supply/SupplyBanner.js
+++ src/components/Supply/SupplyBanner.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import Banner from '../Utils/Banner';
+
 import coinImage from '../../assets/supplytoken.gif';
+
+import Banner from '../Utils/Banner';
+
 import './SupplyBanner.css';
 
 function SupplyBanner() {
   return (
     <Banner component="supply-banner">
-      <div className="front align-center justify-center d-flex-row justify-around w-100">
+      
         <div className="front align-center justify-center d-flex-col">
           <div>
-            <h1>777M</h1>
+            <h1><span>777</span>M</h1>
           </div>
           <div>
             <h2>SUPPLY</h2>
@@ -31,7 +34,7 @@ function SupplyBanner() {
         </div>
 
 
-      </div>
+
 
     </Banner>
   );
