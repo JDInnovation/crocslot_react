125e6e5fc7e20cf033b5378b0a2475092128c913
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-06-05 21:23:39 +0100

    .

--- src/components/Proposal/ProposalBanner.js
+++ src/components/Proposal/ProposalBanner.js
@@ -11,10 +11,10 @@ function ProposalBanner() {
 
       <div className="d-flex-col text-center align-center justify-center justify-between">
         <h1>HOW TO BUY?</h1>
-        <h3>1. Download Phantom wallet</h3>
-        <h3>2. Purchase or Receive $Solana</h3>
-        <h3>3. Head to Jupiter Exchange </h3>
-        <h3>4. Connect wallet and swap for $CROKI</h3>
+        <h3>1. Download <span className="phantom">Phantom</span> wallet</h3>
+        <h3>2. Purchase or Receive <span className="solana">$Solana</span></h3>
+        <h3>3. Head to <span className="jupiter">Jupiter</span> Exchange </h3>
+        <h3>4. Connect wallet and swap for <span className="croki">$CROKI</span></h3>
       </div>
 
 
