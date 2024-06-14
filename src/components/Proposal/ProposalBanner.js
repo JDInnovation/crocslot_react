469727c4b1c50f99895fb387636bdba5d669f2db
Jd.innovation <jdinvestmentgroup@outlook.com>
2024-06-14 21:29:46 +0100

    .

--- src/components/Proposal/ProposalBanner.js
+++ src/components/Proposal/ProposalBanner.js
@@ -12,11 +12,11 @@ function ProposalBanner() {
       <div className="proposal justify-around align-center h-100 ">
         <div className="d-flex-col align-center justify-center text-center text-box ">
           <h1>PROPOSAL</h1>
-          <p>Beyond its viral meme appeal, Croki offers real utility. <br></br>
+          <p>Beyond its viral meme appeal, Croki offers real utility. <br></br><br></br>
 
-            Each token acts as a spin in a decentralized, open-source slot machine, providing fair winnings for gambling enthusiasts.<br></br>
+            Each token acts as a spin in a decentralized, open-source slot machine, providing fair winnings for gambling enthusiasts.<br></br> <br></br>
 
-            Join us and be part of this ambitizous, innovative project that’s set to revolutionize the gabling and meme world!<br></br>
+            Join us and be part of this ambitizous, innovative project that’s set to revolutionize the gabling and meme world!<br></br> <br></br>
           </p>
         </div>
         <div  className="d-flex-col justify-center align-center">
