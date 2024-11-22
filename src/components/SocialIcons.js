93742326641c7340a3fab8c51ededb5ff5df2486
JD Innovation Studio <96195808+JDInnovation@users.noreply.github.com>
2024-11-22 19:51:13 +0000

    Update SocialIcons.js

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -5,9 +5,9 @@ import { FaTelegram, FaXTwitter, FaInstagram, FaYahoo } from 'react-icons/fa6';
 const SocialIcons = () => {
   return (
     <div className="social-icons">
-      <a href="https://t.me/+hlC4qTlwUjYxYTg0" target="_blank"><FaTelegram className="icon" /></a>
-      <a href="https://www.instagram.com/croki_solana?igsh=Mjg5ODh3N2NoMXZ4&utm_source=qr" target="_blank"><FaXTwitter className="icon" /></a>
-      <a href="https://www.instagram.com/croki_solana?igsh=Mjg5ODh3N2NoMXZ4&utm_source=qr" target="_blank"><FaInstagram className="icon" /></a>
+      <a href="https://t.me/+1r2tilv65PwwZmNk" target="_blank"><FaTelegram className="icon" /></a>
+      <a href="https://x.com/CrokiToken" target="_blank"><FaXTwitter className="icon" /></a>
+      <a href="https://x.com/CrokiToken" target="_blank"><FaInstagram className="icon" /></a>
     </div>
   );
 }
