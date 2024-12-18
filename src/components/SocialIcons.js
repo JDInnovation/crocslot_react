5ca81a668caa334661a7cf4531feb7df9dc816b0
JD Innovation Studio <96195808+JDInnovation@users.noreply.github.com>
2024-12-18 23:30:12 +0000

    Update SocialIcons.js

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -7,7 +7,7 @@ const SocialIcons = () => {
     <div className="social-icons">
       <a href="https://t.me/+1r2tilv65PwwZmNk" target="_blank"><FaTelegram className="icon" /></a>
       <a href="https://x.com/CrokiToken" target="_blank"><FaXTwitter className="icon" /></a>
-      <a href="https://x.com/CrokiToken" target="_blank"><FaInstagram className="icon" /></a>
+     
     </div>
   );
 }
